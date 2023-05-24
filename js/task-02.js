c61e6e30df3e43cd2b3df8b4a9d87eec678a1d08
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-05-24 06:00:35 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+for( const el of ingredients){
+ const ulList=document.querySelector('ul');
+ 
+ const ingredient= document.createElement('li')
+ulList.appendChild(ingredient)
+
+
+ ingredient.classList.add('item')
+ ingredient.textContent=el;
+
+ console.log(ingredient)
+}
