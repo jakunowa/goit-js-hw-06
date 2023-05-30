99ad9ffa22bd05437db272fe3df24534fce564dd
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-05-30 07:21:09 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,2 +1,6 @@
 const inputEl=document.querySelector('#font-size-control')
 const spanEl= document.querySelector('#text')
+
+inputEl.addEventListener('input', (e)=>{
+    spanEl.style.fontSize=e.currentTarget.value+'px'
+})
