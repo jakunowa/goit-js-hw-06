5dd821e98735703551ed1cb5e7a59eb3f5259413
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-02 22:46:02 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+const submitHandler = (event) => {
+  event.preventDefault();
+  console.log(event)
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Proszę uzupełnić wsystkie pola");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", submitHandler);
