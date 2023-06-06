6d710edeeea2aa7cf8e541ecb58ec3614983440e
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-06 20:27:10 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,8 @@
-const  inputEl=document.querySelector('#validation-input')
+const inputEl = document.getElementById('validation-input');
+const { length } = inputEl.dataset;
 
-function checkPassword(){
-    if(inputEl.value.length=== +inputEl.dataset.length){
-        inputEl.className='valid'
-    }else{
-        inputEl.className='invalid'
-    }
-}
-checkPassword()
-inputEl.addEventListener('blur', checkPassword)
-
-// const inputEl = document.getElementById('validation-input');
-
-// inputEl.addEventListener('blur', () => {
-//   inputEl.value.length === inputEl.dataset.length
-//     ? (inputEl.className = 'valid')
-//     : (inputEl.className = 'invalid');
-// });
+inputEl.addEventListener('blur', () => {
+  inputEl.value.length === Number(length)
+    ? (inputEl.className = 'valid')
+    : (inputEl.className = 'invalid');
+});
