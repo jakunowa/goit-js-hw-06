d780e38cf9ed00b1b8a8c59daa803e84c706ebff
jakunowa <117861625+jakunowa@users.noreply.github.com>
2023-06-07 22:11:25 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,8 @@ const liItem= document.querySelectorAll(".item")
 console.log(`Number of Categories ${liItem.length}`)
 
 for(const el of liItem){
-    console.log(`Category: ${el.children[0].innerText}`);
-    console.log(`Elements: ${el.children[1].children.length}`)
+    console.log(`Category: ${el.querySelector('h2').innerText}`);
+    console.log(`Elements: ${el.querySelectorAll('li').length}`)
+    
 }
+
